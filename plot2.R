1a7edf7856cbbed4a57b594de11142616eaf5c23
sfbillyd <billydprogramming@gmail.com>
2015-02-05 21:48:17 -0800

    Added plot2.R and plot2.png

--- plot2.R
+++ plot2.R
@@ -4,11 +4,15 @@
 ########
 ## Extract relevant rows from the dataset
 my_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", skip = 66636, nrows = 2880)
-## Create a vector out of the values from the Global Active Power column (3)
-x <- my_data[[3]]
-## Generate a histogram of the values using labels, title, and color per specification
-hist(x, col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
-## Copy my plot to a PNG file
-dev.copy(png, file = "plot1.png")
+## Add column names
+colnames(my_data) <- c("Date", "Time", "Active")
+## Change Date column from character to date
+my_data[, 1] <- as.Date(my_data[, 1], format="%d/%m/%Y")
+## add a column that combines date and time
+my_data$dateTime <- as.POSIXct(paste(my_data$Date, my_data$Time), format="%Y-%m-%d %H:%M:%S")
+## open PNG device
+png(filename = "plot2.png", width = 480, height = 480)
+## Generate a line plot of Global Active Power vs. Day using labels, title, and color per specification
+plot(Active ~ dateTime, my_data, xlab = "", ylab = "Global Active Power (kilowatts)", type = 'l')
 ## Close the PNG device
 dev.off()
